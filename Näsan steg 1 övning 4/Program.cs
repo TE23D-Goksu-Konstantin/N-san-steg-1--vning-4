990acd8ed2289f251135f8a1e7b01d491b979ac8
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-11 09:05:46 +0100

    klar

---
+++ Näsan steg 1 övning 4/Program.cs
@@ -0,0 +1,25 @@
+﻿bool yippi = false;
+string lösenord;
+
+Console.WriteLine("Lösenorddddddd::");
+
+while(yippi == false)
+{
+    lösenord = Console.ReadLine();
+    if(lösenord == "125"){
+        Console.WriteLine("Bra jobbat nu hejdå");
+        Thread.Sleep(2000);
+        yippi = true;
+}
+else if(string.IsNullOrWhiteSpace(lösenord))
+{
+    Console.WriteLine("Skriv någotngint snälla");
+    continue;
+}
+else
+{
+    Console.WriteLine("Feell");
+    continue;
+}
+
+}
